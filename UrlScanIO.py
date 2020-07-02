6a865b2a442ee3fe8b41b368904be4ccc278669b
Shilpesh Trivedi <tshilpesh@gmail.com>
2020-07-02 17:25:57 +0530

    Add files via upload

---
+++ UrlScanIO.py
@@ -0,0 +1,71 @@
+#!/usr/bin/Python2
+#Author Shilpesh Trivedi
+
+
+import sys
+import time
+import json
+import urllib
+import requests
+
+
+class UrlScanIO(object):
+
+	def __init__(self):
+
+		self.key = "402b87ad-e4a1-4220-9d0b-a4f1d7eadd0e"
+		self.base = "https://urlscan.io/api/v1"
+		self.header = {'Content-Type ': 'application/json','API-Key': self.key,}
+		self.urlss = 'https://urlscan.io/screenshots/'
+
+
+	def SubmitURL(self,submiturl):
+
+		url = '/scan/'
+
+		header = {'Content-Type ': 'application/json','API-Key': self.key,}
+
+		data = {'url':submiturl}
+
+		response = requests.post(self.base+url,headers=self.header,data=data)
+
+		OutPut = response.content.decode("utf-8")
+
+		try:
+			
+			Json_Data = json.loads(OutPut)
+
+			uuid = Json_Data['uuid']
+
+			print '\n\t [*] This Sript Will takes only 30 Second to run, OutPut Will Store as Screenshots\n'
+
+
+			time.sleep(30)
+			
+			urllib.urlretrieve(self.urlss+uuid+'.png',uuid+'.png')
+
+			print '\t [*] Please see',uuid+'.png\n'
+			print '\t [*] For More Click ','https://urlscan.io/result/'+uuid,'\n'
+
+		except:
+
+			print '\n\t [*]',Json_Data['description'],'\n'
+
+
+if __name__ == '__main__':
+
+        print ' +++++++++++++++++++++++++++++++++'
+        print ' + Url Scan on UrlScanIO         +'
+        print ' +          By: Shilpesh Trivedi +'
+        print ' +++++++++++++++++++++++++++++++++\n'
+
+        try:
+                Url = sys.argv[1]
+                print '\t [*] Subited Url:',Url
+                UrlScanIO().SubmitURL(Url)
+        except:
+                print '\tUrl\\Domain Must Required as a Argument\n'
+
+#UrlScanIO().SubmitURL("https://urlscan.io/result/0ab7241c-cc9e-48a8-a28a-39a51f562402")
+
+		
