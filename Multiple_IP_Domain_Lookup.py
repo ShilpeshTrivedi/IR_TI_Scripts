782c3841a3dc4078907ff12b15e204c273eca32e
Shilpesh Trivedi <tshilpesh@gmail.com>
2020-10-21 22:09:54 +0530

    Update Multiple_IP_Domain_Lookup.py

--- Multiple_IP_Domain_Lookup.py
+++ Multiple_IP_Domain_Lookup.py
@@ -82,19 +82,17 @@ if __name__ == '__main__':
         exit()
 
     for i,data in enumerate(fp):
+	
+	VT_OP = TI_Obj.VirusTotal(data)
+        IBM_OP = TI_Obj.IBM(data)
 
         if i%4==0 and i!=0:
-		
-		VT_OP = TI_Obj.VirusTotal(data)
-        	IBM_OP = TI_Obj.IBM(data)
 
 		print '\t'+data.rstrip('\n'),'->','VirusTotal Score =',VT_OP,', IBM X-Force Score =',IBM_OP
 		file_cvs.write(data.rstrip('\n')+','+str(VT_OP)+','+str(IBM_OP)+'\n')
 		time.sleep(60)
 
         else:
-		VT_OP = TI_Obj.VirusTotal(data)
-        	IBM_OP = TI_Obj.IBM(data)
 		
             	print '\t'+data.rstrip('\n'),'->','VirusTotal Score =',VT_OP,', IBM X-Force Score =',IBM_OP
             	file_cvs.write(data.rstrip('\n')+','+str(VT_OP)+','+str(IBM_OP)+'\n')
